679352da770feb1ad785a5242779b1f70fca1a2e
bernie <dragcotech@gmail.com>
2024-03-12 08:25:37 +0700

    a

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "c", "lua", "rust", "ruby", "vim", "html", "java" },
+    ensure_installed = { "c", "cpp", "lua", "rust", "ruby", "vim", "html", "css", "javascript", "typescript", "java", "astro", "svelte" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
