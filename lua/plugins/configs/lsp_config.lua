24c796da7ff803dd57e732e34b6a2e6235ce69c0
Dragcotech <dragcotech@gmail.com>
2024-12-11 00:17:59 +0700

    a

--- lua/plugins/configs/lsp_config.lua
+++ lua/plugins/configs/lsp_config.lua
@@ -1,5 +1,5 @@
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "solargraph", "tsserver" }
+    ensure_installed = { "lua_ls", "ts_ls" }
 })
 
 local lspconfig = require('lspconfig')
