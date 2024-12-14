58e6cb64d432f46f7029cbe675fabef935531c7a
Dragcotech <dragcotech@gmail.com>
2024-12-14 14:43:49 +0700

    a

--- init.lua
+++ init.lua
@@ -1,18 +1,5 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 -- config basic
 require "core"
@@ -31,5 +18,3 @@ require("plugins.configs.nvimtree")
 require("plugins.configs.telescope")
 -- default theme
 vim.cmd.colorscheme("gruvbox")
-
-
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,20 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({
   -- treesitter
   -- { 'nvim-treesitter/nvim-treesitter' },
@@ -23,7 +40,7 @@ require("lazy").setup({
   "neovim/nvim-lspconfig",
   -- telescope
   "nvim-lua/plenary.nvim",
-  { 
+  {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { 'nvim-lua/plenary.nvim' }
@@ -38,4 +55,5 @@ require("lazy").setup({
   --{ 'projekt0n/github-nvim-theme', name = 'github-theme' },
 })
 
-
+-- default theme
+vim.cmd.colorscheme("gruvbox")
