39d7ed6d40cdf14de3ae5884673cbfd8d8b8a250
Dragcotech <dragcotech@gmail.com>
2024-12-15 10:25:21 +0700

    a

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,7 +17,11 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     -- BufferLine
-    { 'akinsho/bufferline.nvim',             version = "*", dependencies = 'nvim-tree/nvim-web-devicons' },
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons'
+    },
     -- Tree
     {
         "nvim-tree/nvim-tree.lua",
@@ -32,7 +36,12 @@ require("lazy").setup({
     'lewis6991/gitsigns.nvim',
     -- Lualine
     'nvim-lualine/lualine.nvim', -- Fancier statusline
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl",  opts = {},                                   enabled = false },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {},
+        enabled = false
+    },
     {
         'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
         event = { "BufRead", "BufNewFile" },
@@ -63,56 +72,10 @@ require("lazy").setup({
         dependencies = {
             'hrsh7th/cmp-nvim-lsp',
             'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip'
+            'hrsh7th/cmp-buffer',
+            'saadparwaiz1/cmp_luasnip',
+            'rafamadriz/friendly-snippets',
         },
-        config = function()
-            -- nvim-cmp setup
-            local cmp = require 'cmp'
-            local luasnip = require 'luasnip'
-
-            cmp.setup({
-                view = {
-                    entries = "native"
-                },
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert {
-                    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<CR>'] = cmp.mapping.confirm {
-                        behavior = cmp.ConfirmBehavior.Replace,
-                        select = true,
-                    },
-                    ['<Tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { 'i', 's' }),
-                    ['<S-Tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { 'i', 's' }),
-                },
-                sources = {
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                    { name = "neorg" },
-                },
-            })
-        end
     },
     -- Telescope
     {
@@ -122,7 +85,11 @@ require("lazy").setup({
     },
     'nvim-telescope/telescope-symbols.nvim',
     -- Theme
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000
+    },
     "ellisonleao/gruvbox.nvim",
     -- {
     --   'folke/tokyonight.nvim',
