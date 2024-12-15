c24c7ff01a841039a9d822b1f81f7c3c1dca0f58
Dragcotech <dragcotech@gmail.com>
2024-12-15 11:57:07 +0700

    a

--- lua/plugins/configs/terminal.lua
+++ lua/plugins/configs/terminal.lua
@@ -42,7 +42,7 @@ require('toggleterm').setup({
     terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
     persist_size = true,
     persist_mode = true,      -- if set to true (default) the previous terminal mode will be remembered
-    direction = 'tab',
+    direction = 'vertical',
     close_on_exit = true,     -- close the terminal window when the process exits
     clear_env = false,        -- use only environmental variables from `env`, passed to jobstart()
     -- Change the default shell. Can be a string or a function returning a string
