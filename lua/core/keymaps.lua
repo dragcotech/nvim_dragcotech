1f214e56f8d4d9fb3c4603bd800ad81114625c0d
Dragcotech <dragcotech@gmail.com>
2025-01-04 11:33:27 +0700

    a

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,6 +5,13 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
+-- terminal
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
+vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-w>h]], { noremap = true, silent = true })
+vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-w>j]], { noremap = true, silent = true })
+vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-w>k]], { noremap = true, silent = true })
+vim.keymap.set('t', '<C-l>', [[<C-\><C-n><C-w>l]], { noremap = true, silent = true })
+
 -- split
 vim.keymap.set('n', '<leader>sh', ':split<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>sv', ':vsplit<CR>', { noremap = true, silent = true })
