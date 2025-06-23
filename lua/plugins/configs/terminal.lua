450bb9eaf1c982441581231eb053522fd0596ca9
dragcotech <dragcotech@gmail.com>
2025-06-23 18:41:38 +0700

    A

--- lua/plugins/configs/terminal.lua
+++ lua/plugins/configs/terminal.lua
@@ -1,9 +1,12 @@
 return {
     'akinsho/toggleterm.nvim',
     version = "*",
-    config = true,
 
     config = function()
+        -- Mapping trong terminal mode để gửi Ctrl+C
+        vim.api.nvim_set_keymap('t', '<C-x>', [[<C-\><C-n>:call chansend(b:terminal_job_id, "\x03")<CR>i]],
+            { noremap = true, silent = true })
+
         require('toggleterm').setup({
             -- shell = vim.fn.executable("zsh") and "zsh" or vim.o.shell,
             -- size can be a number or function which is passed the current terminal
@@ -82,5 +85,19 @@ return {
                 horizontal_breakpoint = 135,
             }
         })
+
+        function _G.set_terminal_keymaps()
+            local opts = { buffer = 0 }
+            vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+            vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+            vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+            vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+            vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+            vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+            vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+        end
+
+        -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+        vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
     end
 }
