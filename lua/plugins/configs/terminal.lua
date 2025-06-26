c2407bfb0387e6fd457a9f48038b1ae300569ea5
dragcotech <dragcotech@gmali.com>
2025-06-26 20:18:33 +0700

    a

--- lua/plugins/configs/terminal.lua
+++ lua/plugins/configs/terminal.lua
@@ -51,7 +51,7 @@ return {
             terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
             persist_size = true,
             persist_mode = true,      -- if set to true (default) the previous terminal mode will be remembered
-            direction = 'vertical',
+            direction = 'horizontal',
             close_on_exit = true,     -- close the terminal window when the process exits
             clear_env = false,        -- use only environmental variables from `env`, passed to jobstart()
             -- Change the default shell. Can be a string or a function returning a string
