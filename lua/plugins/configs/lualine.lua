fc4ffc04091afbf5e965608a764f650e1783b6de
dragcotech <dragcotech@gmail.com>
2025-06-30 00:13:38 +0700

    A

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -1,13 +1,14 @@
 return {
     'nvim-lualine/lualine.nvim', -- Fancier statusline
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    'tpope/vim-sleuth',          -- Detect tabstop and shiftwidth automatically
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'tpope/vim-sleuth', },
 
     config = function()
         require('lualine').setup {
             options = {
                 icons_enabled = true,
-                theme = 'gruvbox',
+                theme = 'auto',
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
             },
             sections = {
                 lualine_a = {
