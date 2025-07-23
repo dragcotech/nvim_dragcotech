c4a8b26b38306a00985c0f83381fb308ac590b23
dragcotech <dragcotech@gmail.com>
2025-07-24 03:01:59 +0700

    first commit

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -13,7 +13,7 @@ return {
         require("nvim-tree").setup({
             sort_by = "case_sensitive",
             view = {
-                width = 40,
+                width = 30,
             },
             renderer = {
                 group_empty = true,
